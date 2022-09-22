804de7c6d0288769e0dea507b9ba157b6113e872
Postmodern <postmodern.mod3@gmail.com>
2022-09-22 07:02:26 -0700

    Added `OpenRedirect` (closes #10).

---
+++ lib/ronin/vulns/open_redirect.rb
@@ -0,0 +1,94 @@
+#
+# ronin-vulns - A Ruby library for blind vulnerability testing.
+#
+# Copyright (c) 2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-vulns is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-vulns is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-vulns.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/vulns/web'
+
+require 'chars'
+require 'cgi'
+
+module Ronin
+  module Vulns
+    #
+    # Represents an Open Redirect vulnerability.
+    #
+    # ## Features
+    #
+    # * Checks 301, 302, 303, 307, and 308 HTTP redirects.
+    # * Checks `meta` refresh redirects.
+    # * Includes random alpha-numeric data in the test values.
+    #
+    class OpenRedirect < Web
+
+      # The desired redirect URL to use in the test.
+      #
+      # @return [String]
+      attr_reader :test_url
+
+      #
+      # Initializes the Open Redirect vulnerability.
+      #
+      # @param [String] test_url
+      #   The desired redirect URL to test the URL with.
+      #
+      def initialize(url, test_url: self.class.random_test_url, **kwargs)
+        super(url,**kwargs)
+
+        @test_url = test_url
+      end
+
+      #
+      # Generates a random redirect URL to use in tests.
+      #
+      # @return [String]
+      #   A random URL to https://ronin-rb.dev/vulns/open_redirect.html.
+      #
+      # @api private
+      #
+      def self.random_test_url
+        "https://ronin-rb.dev/vulns/open_redirect.html?id=#{Chars::ALPHA_NUMERIC.random_string(5)}"
+      end
+
+      #
+      # Tests whther the URL has a vulnerable Open Redirect.
+      #
+      # @return [Boolean]
+      #
+      def vulnerable?
+        response = exploit(@test_url)
+
+        case response.code
+        when '301', '302', '303', '307', '308'
+          if (locations = response.get_fields('Location'))
+            locations.last == @test_url
+          end
+        else
+          content_type = response.content_type
+
+          if content_type && content_type.include?('text/html')
+            escaped_test_url = Regexp.escape(CGI.escapeHTML(@test_url))
+            regexp = %r{<meta\s+http-equiv=(?:"refresh"|'refresh'|refresh)\s+content=(?:"\d+;\s*url='#{escaped_test_url}'"|'\d+;\s*url="#{escaped_test_url}"'|\d+;url=(?:"#{escaped_test_url}"|'#{escaped_test_url}'))\s*/>}i
+
+            response.body =~ regexp
+          end
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/open_redirect_spec.rb
@@ -0,0 +1,449 @@
+require 'spec_helper'
+require 'web_vuln_examples'
+require 'ronin/vulns/open_redirect'
+
+require 'webmock/rspec'
+
+describe Ronin::Vulns::OpenRedirect do
+  describe "#initialize" do
+    include_examples "Ronin::Vulns::Web#initialize examples"
+
+    it "must set #test_url" do
+      expect(subject.test_url).to match(%r{\Ahttps://ronin-rb\.dev/vulns/open_redirect\.html\?id=[A-Za-z0-9]+\z})
+    end
+  end
+
+  describe ".random_test_url" do
+    subject { described_class }
+
+    it "must return a random 'https://ronin-rb.dev/vulns/open_redirect.html?id=...' URL" do
+      expect(subject.random_test_url).to match(%r{\Ahttps://ronin-rb\.dev/vulns/open_redirect\.html\?id=[A-Za-z0-9]+\z})
+    end
+
+    it "must return a random test URL each time" do
+      urls = Array.new(3) { subject.random_test_url }
+
+      expect(urls.uniq.length).to be > 1
+    end
+  end
+
+  let(:query_param) { 'bar' }
+  let(:url)         { "https://example.com/page?foo=1&bar=2&baz=3" }
+
+  subject { described_class.new(url, query_param: query_param) }
+
+  describe "#vulnerable?" do
+    let(:request_url) { subject.exploit_url(subject.test_script_url) }
+
+    let(:response_body) do
+      <<~HTML
+        <html>
+          <body>
+            <p>example content</p>
+            <p>included content</p>
+            <p>more content</p>
+          </body>
+        </html>
+      HTML
+    end
+
+    let(:code) { '200' }
+    let(:response) do
+      double('Net::HTTPResponse', content_type: 'text/html',
+                                  code:         code,
+                                  body:         response_body)
+    end
+
+    before do
+      expect(subject).to receive(:exploit).with(subject.test_url).and_return(response)
+    end
+
+    it "must call #exploit with #test_url" do
+      subject.vulnerable?
+    end
+
+    %w[301 302 303 307 308].each do |code|
+      context "when the response code is 301" do
+        let(:code) { code }
+
+        let(:response) do
+          double('Net::HTTPResponse', code: code)
+        end
+
+        context "and there is a Location header" do
+          context "and it equals #test_url" do
+            let(:location) { subject.test_url }
+
+            before do
+              allow(response).to receive(:get_fields).with('Location').and_return([location])
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "but it does not match #test_url" do
+            let(:location) { "https://example.com/" }
+
+            before do
+              allow(response).to receive(:get_fields).with('Location').and_return([location])
+            end
+
+            it "must return false" do
+              expect(subject.vulnerable?).to be_falsy
+            end
+          end
+        end
+
+        context "but there are multiple Location headers" do
+          context "and the last Location header is the #test_url" do
+            let(:locations) do
+              ['http://example.com/', subject.test_url]
+            end
+
+            before do
+              allow(response).to receive(:get_fields).with('Location').and_return(locations)
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "but the last Location header is not #test_url" do
+            let(:locations) do
+              ['http://example.com/foo', "https://example.com/bar"]
+            end
+
+            before do
+              allow(response).to receive(:get_fields).with('Location').and_return(locations)
+            end
+
+            it "must return false" do
+              expect(subject.vulnerable?).to be_falsy
+            end
+          end
+        end
+
+        context "but there is no Location header" do
+          before do
+            allow(response).to receive(:get_fields).with('Location').and_return(nil)
+          end
+
+          it "must return false" do
+            expect(subject.vulnerable?).to be_falsy
+          end
+        end
+      end
+    end
+
+    context "when the response code isn't a 30x" do
+      let(:code) { '200' }
+
+      let(:response) do
+        double('Net::HTTPResponse', code: code, body: response_body)
+      end
+
+      context "when the response Content-Type includes 'text/html'" do
+        let(:content_type) { "text/html; charset=UTF-8" }
+        let(:response) do
+          double('Net::HTTPResponse', code:         code,
+                                      content_type: content_type,
+                                      body:         response_body)
+        end
+
+        context "and the response includes a meta refresh redirect" do
+          let(:response_body) do
+            <<~HTML
+            <html>
+              <head>
+                <meta http-equiv="refresh" content="0;url='#{subject.test_url}'"/>
+              </head>
+              <body>
+                <p>example content</p>
+                <p>included content</p>
+                <p>more content</p>
+              </body>
+            </html>
+            HTML
+          end
+
+          it "must return true" do
+            expect(subject.vulnerable?).to be_truthy
+          end
+
+          context "when meta tag is all upercase" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <META HTTP-EQUIV="REFRESH" CONTENT="0;URL='#{subject.test_url}'"/>
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "when the http-equiv attribute is single quoted" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <meta http-equiv='refresh' content="10;url='#{subject.test_url}'"/>
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "when the http-equiv attribute is not quoted" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <meta http-equiv=refresh content="10;url='#{subject.test_url}'"/>
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "when the meta refresh has a delay value" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <meta http-equiv="refresh" content="10;url='#{subject.test_url}'"/>
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "when there is a space between the delay value and the url value" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <meta http-equiv="refresh" content="10; url='#{subject.test_url}'"/>
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "when there is a newline between the delay value and the url value" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <meta http-equiv="refresh" content="10;
+url='#{subject.test_url}'"/>
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "when the url name is uppercase" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <meta http-equiv="refresh" content="0;URL='#{subject.test_url}'"/>
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "when the http-equiv attribute is single quoted" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <meta http-equiv='refresh' content="0;url='#{subject.test_url}'"/>
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+
+          context "when the content attribute is single quoted" do
+            context "and the url value is double quoted" do
+              let(:response_body) do
+                <<~HTML
+                <html>
+                  <head>
+                    <meta http-equiv="refresh" content='0;url="#{subject.test_url}"'/>
+                  </head>
+                  <body>
+                    <p>example content</p>
+                    <p>included content</p>
+                    <p>more content</p>
+                  </body>
+                </html>
+                HTML
+              end
+
+              it "must return true" do
+                expect(subject.vulnerable?).to be_truthy
+              end
+            end
+          end
+
+          context "when the content attribute is not quoted" do
+            context "and the url value is double quoted" do
+              let(:response_body) do
+                <<~HTML
+                <html>
+                  <head>
+                    <meta http-equiv="refresh" content=0;url="#{subject.test_url}"/>
+                  </head>
+                  <body>
+                    <p>example content</p>
+                    <p>included content</p>
+                    <p>more content</p>
+                  </body>
+                </html>
+                HTML
+              end
+
+              it "must return true" do
+                expect(subject.vulnerable?).to be_truthy
+              end
+            end
+
+            context "and the url value is single quoted" do
+              let(:response_body) do
+                <<~HTML
+                <html>
+                  <head>
+                    <meta http-equiv="refresh" content=0;url='#{subject.test_url}'/>
+                  </head>
+                  <body>
+                    <p>example content</p>
+                    <p>included content</p>
+                    <p>more content</p>
+                  </body>
+                </html>
+                HTML
+              end
+
+              it "must return true" do
+                expect(subject.vulnerable?).to be_truthy
+              end
+            end
+          end
+
+          context "when there is a space before the '/>'" do
+            let(:response_body) do
+              <<~HTML
+              <html>
+                <head>
+                  <meta http-equiv="refresh" content="0;url='#{subject.test_url}'" />
+                </head>
+                <body>
+                  <p>example content</p>
+                  <p>included content</p>
+                  <p>more content</p>
+                </body>
+              </html>
+              HTML
+            end
+
+            it "must return true" do
+              expect(subject.vulnerable?).to be_truthy
+            end
+          end
+        end
+      end
+
+      context "but the response Content-Type does not contain 'text/html'" do
+        let(:content_type) { "text/plain" }
+        let(:response) do
+          double('Net::HTTPResponse', code:         code,
+                                      content_type: content_type,
+                                      body:         response_body)
+        end
+        
+        it "must return false" do
+          expect(subject.vulnerable?).to be_falsy
+        end
+      end
+    end
+  end
+end
