b450b1df38bff9a5de5080af67d618769046931d
Postmodern <postmodern.mod3@gmail.com>
2022-09-22 21:30:21 -0700

    Added some CSS to the RFI test script in case anyone manually views the result.

--- data/rfi_test.php
+++ data/rfi_test.php
@@ -1,1 +1,24 @@
-<?php echo strrev("!detceteD )IFR( noisulcnI eliF etomeR :trelA ytiruceS") . PHP_EOL; ?>
+<?php
+echo <<<EOS
+<style type="text/css">
+#rfi-security-alert {
+  position: relative;
+  margin: 25vh 25vw 25vh 25vw;
+  padding: 5em;
+  color: black;
+  background-color: white;
+  border: 4em solid red;
+  z-index: 10000;
+}
+#rfi-security-alert p {
+  text-align: center;
+  font-weight: bold;
+  font-size: 4em;
+}
+</style>
+EOS . PHP_EOL;
+
+echo "<div id=\"rfi-security-alert\">" . PHP_EOL;
+echo "  <p>" . strrev("!detceteD )IFR( noisulcnI eliF etomeR :trelA ytiruceS") . "</p>" . PHP_EOL;
+echo "</div>" . PHP_EOL;
+?>
