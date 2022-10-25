9f38bf4348ee89d8df30daf2a8e69401ca549bc5
Postmodern <postmodern.mod3@gmail.com>
2022-10-24 19:47:15 -0700

    Added `CLI::Command`.

---
+++ lib/ronin/vulns/cli/command.rb
@@ -0,0 +1,36 @@
+#
+# ronin-vulns - A Ruby library for blind vulnerability testing.
+#
+# Copyright (c) 2007-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-vulns is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-vulns is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-vulns.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/vulns/root'
+require 'ronin/core/cli/command'
+
+module Ronin
+  module Vulns
+    class CLI
+      #
+      # Base class for all `ronin-vulns` commands.
+      #
+      class Command < Core::CLI::Command
+
+        man_dir File.join(ROOT,'man')
+
+      end
+    end
+  end
+end
