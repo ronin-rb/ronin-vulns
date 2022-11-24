26f3c239a1d6408980df3d777b7a7860e6e7b8e5
Postmodern <postmodern.mod3@gmail.com>
2022-11-24 09:01:54 -0800

    Fixed project name.

--- lib/ronin/vulns/web_vuln.rb
+++ lib/ronin/vulns/web_vuln.rb
@@ -1,20 +1,20 @@
 #
-# ronin-vuln - A Ruby library for blind vulnerability testing.
+# ronin-vulns - A Ruby library for blind vulnerability testing.
 #
 # Copyright (c) 2022 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
-# ronin-vuln is free software: you can redistribute it and/or modify
+# ronin-vulns is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ronin-vuln is distributed in the hope that it will be useful,
+# ronin-vulns is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with ronin-vuln.  If not, see <https://www.gnu.org/licenses/>.
+# along with ronin-vulns.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 require 'ronin/vulns/vuln'
