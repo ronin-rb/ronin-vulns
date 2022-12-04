b6a0be24265216bbf4b5f64b0406593b030c6c38
Postmodern <postmodern.mod3@gmail.com>
2022-12-03 19:03:01 -0800

    Added `WebVuln::HTTPRequest` (issues #29,#30).

---
+++ lib/ronin/vulns/web_vuln/http_request.rb
@@ -0,0 +1,222 @@
+# frozen_string_literal: true
+#
+# ronin-vulns - A Ruby library for blind vulnerability testing.
+#
+# Copyright (c) 2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-vulns is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-vulns is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-vulns.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/vulns/vuln'
+require 'ronin/support/network/http/cookie'
+
+require 'uri/query_params'
+
+module Ronin
+  module Vulns
+    class WebVuln < Vuln
+      #
+      # Represents a HTTP request.
+      #
+      # @api private
+      #
+      class HTTPRequest
+
+        # The URL of the request.
+        #
+        # @return [URI::HTTP]
+        attr_reader :url
+
+        # The HTTP request method.
+        #
+        # @return [:copy, :delete, :get, :head, :lock, :mkcol, :move,
+        #         :options, :patch, :post, :propfind, :proppatch, :put,
+        #         :trace, :unlock]
+        attr_reader :request_method
+
+        # The user to authenticate as.
+        #
+        # @return [String, nil]
+        attr_reader :user
+
+        # The password to authenticate with.
+        #
+        # @return [String, nil]
+        attr_reader :password
+
+        # The optional HTTP `Referer` header for the request.
+        #
+        # @return [String, nil]
+        attr_reader :referer
+
+        # The query param for the request.
+        #
+        # @return [Hash{String,Symbol => String}, nil]
+        attr_reader :query_params
+
+        # Additional `Cookie` header for the request.
+        #
+        # @return [Ronin::Support::Network::HTTP::Cookie, nil]
+        attr_reader :cookie
+
+        # Additional HTTP header names and values to add to the request.
+        #
+        # @return [Hash{Symbol,String => String}, nil]
+        attr_reader :headers
+
+        # The form data that may be sent in the body of the request.
+        #
+        # @return [Hash{String => Object}, nil]
+        attr_reader :form_data
+
+        #
+        # Initializes the HTTP request object.
+        #
+        # @param [URI::HTTP] url
+        #   The URL to test or exploit.
+        #
+        # @param [:copy, :delete, :get, :head, :lock, :mkcol, :move,
+        #         :options, :patch, :post, :propfind, :proppatch, :put,
+        #         :trace, :unlock] request_method
+        #   The HTTP request mehtod for each request.
+        #
+        # @param [String, nil] user
+        #   The user to authenticate as.
+        #
+        # @param [String, nil] password
+        #   The password to authenticate with.
+        #
+        # @param [Hash{Symbol,String => String}, nil] query_params
+        #   Additional URL query params for the request.
+        #
+        # @param [Hash{Symbol,String => String}, nil] headers
+        #   Additional HTTP header names and values to add to the request.
+        #
+        # @param [String, Hash{String => String}, nil] cookie
+        #   Additional `Cookie` header for the request..
+        #
+        # @param [Hash, nil] form_data
+        #   The form data that may be sent in the body of the request.
+        #
+        def initialize(url, request_method: :get,
+                            user:           nil,
+                            password:       nil,
+                            referer:        nil,
+                            query_params:   nil,
+                            headers:        nil,
+                            cookie:         nil,
+                            form_data:      nil)
+          @url = url
+          
+          if query_params && !query_params.empty?
+            @url = url.dup
+            @url.query_params = query_params
+          end
+
+          @request_method = request_method
+          @user           = user
+          @password       = password
+          @referer        = referer
+
+          @query_params = query_params
+          @cookie       = if cookie
+                            Support::Network::HTTP::Cookie.new(cookie)
+                          end
+          @headers      = headers
+          @form_data    = form_data
+        end
+
+        #
+        # Converts the HTTP request to a `curl` command.
+        #
+        # @return [String]
+        #
+        def to_curl
+          escape = ->(str) { "'#{str.to_s.tr("'","\\'")}'" }
+
+          command = ['curl']
+
+          if @request_method != :get
+            command << '--request' << @request_method.upcase
+          end
+
+          if (@user || @password)
+            command << '--user' << escape.call("#{@user}:#{@password}")
+          end
+
+          if @referer
+            command << '--referer' << escape.call(@referer)
+          end
+
+          if (@cookie && !@cookie.empty?)
+            command << '--cookie' << escape.call(@cookie)
+          end
+
+          if @headers
+            @headers.each do |name,value|
+              command << '--header' << escape.call("#{name}: #{value}")
+            end
+          end
+
+          if (@form_data && !@form_data.empty?)
+            form_string = URI.encode_www_form(@form_data)
+            command << '--form-string' << escape.call(form_string)
+          end
+
+          command << escape.call(@url)
+
+          return command.join(' ')
+        end
+
+        # HTTP newline deliminator.
+        CRLF = "\r\n"
+
+        #
+        # Converts the HTTP request to a raw HTTP request.
+        #
+        # @return [String]
+        #
+        def to_http
+          request = []
+          request << "#{@request_method.upcase} #{@url.request_uri} HTTP/1.1"
+
+          if (@form_data && !@form_data.empty?)
+            request << "Content-Type: x-www-form-urlencoded"
+          end
+
+          if (@user || @password)
+            basic_auth = ["#{@user}:#{@password}"].pack('m0')
+            request << "Authorization: Basic #{basic_auth}"
+          end
+
+          request << "Referer: #{@referer}" if @referer
+          request << "Cookie: #{@cookie}"   if (@cookie && !@cookie.empty?)
+
+          if @headers
+            @headers.each do |name,value|
+              request << "#{name}: #{value}"
+            end
+          end
+
+          if (@form_data && !@form_data.empty?)
+            request << ''
+            request << URI.encode_www_form(@form_data)
+          end
+
+          return request.join(CRLF) << CRLF
+        end
+      end
+    end
+  end
+end
---
+++ spec/web_vuln/http_request_spec.rb
@@ -0,0 +1,350 @@
+require 'spec_helper'
+require 'ronin/vulns/web_vuln/http_request'
+
+describe Ronin::Vulns::WebVuln::HTTPRequest do
+  let(:url) { URI.parse('https://example.com/page.php?id=1') }
+
+  let(:request_method) { :post }
+  let(:user)           { 'bob' }
+  let(:password)       { 'secret' }
+  let(:referer)        { 'https://example.com/' }
+  let(:query_params)   { {'id' => '1', 'foo' => 'bar'} }
+  let(:cookie)         { {'foo' => 'bar', 'bar' => 'baz'} }
+  let(:headers)        { {'X-Foo' => 'bar', 'X-Bar' => 'baz'} }
+  let(:form_data)      { {'id' => '1', 'foo' => 'bar'} }
+
+  subject { described_class.new(url) }
+
+  describe "#initialize" do
+    it "must set #url" do
+      expect(subject.url).to be(url)
+    end
+
+    it "must default #request_method to :get" do
+      expect(subject.request_method).to be(:get)
+    end
+
+    it "must default #user to nil" do
+      expect(subject.user).to be(nil)
+    end
+
+    it "must default #password to nil" do
+      expect(subject.password).to be(nil)
+    end
+
+    it "must default #referer to nil" do
+      expect(subject.referer).to be(nil)
+    end
+
+    it "must default #query_params to nil" do
+      expect(subject.query_params).to be(nil)
+    end
+
+    it "must default #cookie to nil" do
+      expect(subject.cookie).to be(nil)
+    end
+
+    it "must default #headers to nil" do
+      expect(subject.headers).to be(nil)
+    end
+
+    it "must default #form_data to nil" do
+      expect(subject.form_data).to be(nil)
+    end
+
+    context "when the `request_method:` keyword is given" do
+      subject { described_class.new(url, request_method: request_method) }
+
+      it "must set #request_method" do
+        expect(subject.request_method).to be(request_method)
+      end
+    end
+
+    context "when the `user:` keyword is given" do
+      subject { described_class.new(url, user: user) }
+
+      it "must set #user" do
+        expect(subject.user).to be(user)
+      end
+    end
+
+    context "when the `password:` keyword is given" do
+      subject { described_class.new(url, password: password) }
+
+      it "must set #password" do
+        expect(subject.password).to be(password)
+      end
+    end
+
+    context "when the `referer:` keyword is given" do
+      subject { described_class.new(url, referer: referer) }
+
+      it "must set #referer" do
+        expect(subject.referer).to be(referer)
+      end
+    end
+
+    context "when the `query_params:` keyword is given" do
+      subject { described_class.new(url, query_params: query_params) }
+
+      it "must set #query_params" do
+        expect(subject.query_params).to be(query_params)
+      end
+
+      it "must set the #query_params of #url" do
+        expect(subject.url.query_params).to eq(query_params)
+      end
+    end
+
+    context "when the `cookie:` keyword is given" do
+      subject { described_class.new(url, cookie: cookie) }
+
+      it "must initialize #cookie to a Ronin::Support::Network::HTTP::Cookie" do
+        expect(subject.cookie).to be_kind_of(Ronin::Support::Network::HTTP::Cookie)
+      end
+
+      it "must populate #cookie with the cookie values" do
+        expect(subject.cookie.params).to eq(cookie)
+      end
+    end
+
+    context "when the `headers:` keyword is given" do
+      subject { described_class.new(url, headers: headers) }
+
+      it "must set #headers" do
+        expect(subject.headers).to be(headers)
+      end
+    end
+
+    context "when the `form_data:` keyword is given" do
+      subject { described_class.new(url, form_data: form_data) }
+
+      it "must set #form_data" do
+        expect(subject.form_data).to be(form_data)
+      end
+    end
+  end
+
+  describe "#to_curl" do
+    it "must return \"curl 'URL'\" in the command" do
+      expect(subject.to_curl).to eq("curl '#{url}'")
+    end
+
+    context "when #request_method is not :get" do
+      subject { described_class.new(url, request_method: :put) }
+
+      it "must include '--request METHOD' in the command" do
+        expect(subject.to_curl).to eq("curl --request PUT '#{url}'")
+      end
+    end
+
+    context "when #user is set" do
+      subject { described_class.new(url, user: user) }
+
+      it "must include \"--user 'user:'\" in the command" do
+        expect(subject.to_curl).to eq("curl --user '#{user}:' '#{url}'")
+      end
+    end
+
+    context "when #password is set" do
+      subject { described_class.new(url, password: password) }
+
+      it "must include \"--user ':password'\" in the command" do
+        expect(subject.to_curl).to eq("curl --user ':#{password}' '#{url}'")
+      end
+    end
+
+    context "when #user and #password are set" do
+      subject { described_class.new(url, user: user, password: password) }
+
+      it "must include \"--user 'user:password'\" in the command" do
+        expect(subject.to_curl).to eq("curl --user '#{user}:#{password}' '#{url}'")
+      end
+    end
+
+    context "when #referer is set" do
+      subject { described_class.new(url, referer: referer) }
+
+      it "must include \"--referer '...'\" in the command" do
+        expect(subject.to_curl).to eq("curl --referer '#{referer}' '#{url}'")
+      end
+    end
+
+    context "when initialized with the `query_params:` keyword argument" do
+      subject { described_class.new(url, query_params: query_params) }
+
+      it "must merge the query params into in the URL" do
+        merged_url = url.dup
+        merged_url.query_params = query_params
+
+        expect(subject.to_curl).to eq("curl '#{merged_url}'")
+      end
+    end
+
+    context "when #cookies is not empty" do
+      subject { described_class.new(url, cookie: cookie) }
+
+      it "must include \"--cookie '...'\" in the command" do
+        expect(subject.to_curl).to eq("curl --cookie '#{subject.cookie}' '#{url}'")
+      end
+    end
+
+    context "when #headers is not empty" do
+      subject { described_class.new(url, headers: headers) }
+
+      it "must include \"--header 'name=value' ...\" in the command" do
+        expect(subject.to_curl).to eq("curl --header '#{headers.keys[0]}: #{headers.values[0]}' --header '#{headers.keys[1]}: #{headers.values[1]}' '#{url}'")
+      end
+    end
+
+    context "when #form_data is not empty" do
+      subject { described_class.new(url, form_data: form_data) }
+
+      it "must include \"--form-string '...'\" in the command" do
+        encoded_form_data = URI.encode_www_form(form_data)
+
+        expect(subject.to_curl).to eq("curl --form-string '#{encoded_form_data}' '#{url}'")
+      end
+    end
+  end
+
+  describe "#to_http" do
+    it "must return \"GET /path?query_params... HTTP/1.1\" in the command" do
+      expect(subject.to_http).to eq(
+        "GET #{url.request_uri} HTTP/1.1\r\n"
+      )
+    end
+
+    context "when #request_method is not :get" do
+      subject { described_class.new(url, request_method: :put) }
+
+      it "must change the request method" do
+        expect(subject.to_http).to eq(
+          "PUT #{url.request_uri} HTTP/1.1\r\n"
+        )
+      end
+    end
+
+    context "when #user is set" do
+      subject { described_class.new(url, user: user) }
+
+      it "must include the 'Authorization: Basic ...' header" do
+        basic_auth = ["#{user}:"].pack('m0')
+
+        expect(subject.to_http).to eq(
+          [
+            "GET #{url.request_uri} HTTP/1.1",
+            "Authorization: Basic #{basic_auth}",
+            ''
+          ].join("\r\n")
+        )
+      end
+    end
+
+    context "when #password is set" do
+      subject { described_class.new(url, password: password) }
+
+      it "must include the 'Authorization: Basic ...' header" do
+        basic_auth = [":#{password}"].pack('m0')
+
+        expect(subject.to_http).to eq(
+          [
+            "GET #{url.request_uri} HTTP/1.1",
+            "Authorization: Basic #{basic_auth}",
+            ''
+          ].join("\r\n")
+        )
+      end
+    end
+
+    context "when #user and #password are set" do
+      subject { described_class.new(url, user: user, password: password) }
+
+      it "must include the 'Authorization: Basic ...' header" do
+        basic_auth = ["#{user}:#{password}"].pack('m0')
+
+        expect(subject.to_http).to eq(
+          [
+            "GET #{url.request_uri} HTTP/1.1",
+            "Authorization: Basic #{basic_auth}",
+            ''
+          ].join("\r\n")
+        )
+      end
+    end
+
+    context "when #referer is set" do
+      subject { described_class.new(url, referer: referer) }
+
+      it "must include the 'Referer: ...' header" do
+        expect(subject.to_http).to eq(
+          [
+            "GET #{url.request_uri} HTTP/1.1",
+            "Referer: #{referer}",
+            ''
+          ].join("\r\n")
+        )
+      end
+    end
+
+    context "when initialized with the `query_params:` keyword argument" do
+      subject { described_class.new(url, query_params: query_params) }
+
+      it "must merge the query params into in the URL" do
+        merged_url = url.dup
+        merged_url.query_params = query_params
+
+        expect(subject.to_http).to eq(
+          "GET #{merged_url.request_uri} HTTP/1.1\r\n"
+        )
+      end
+    end
+
+    context "when #cookies is not empty" do
+      subject { described_class.new(url, cookie: cookie) }
+
+      it "must include the 'Cookie: ...' header" do
+        expect(subject.to_http).to eq(
+          [
+            "GET #{url.request_uri} HTTP/1.1",
+            "Cookie: #{subject.cookie}",
+            ''
+          ].join("\r\n")
+        )
+      end
+    end
+
+    context "when #headers is not empty" do
+      subject { described_class.new(url, headers: headers) }
+
+      it "must include the header names and values" do
+        expect(subject.to_http).to eq(
+          [
+            "GET #{url.request_uri} HTTP/1.1",
+            "#{headers.keys[0]}: #{headers.values[0]}",
+            "#{headers.keys[1]}: #{headers.values[1]}",
+            ''
+          ].join("\r\n")
+        )
+      end
+    end
+
+    context "when #form_data is not empty" do
+      subject { described_class.new(url, form_data: form_data) }
+
+      it "must include the 'Content-Type: x-www-form-urlencoded' header and form body" do
+        encoded_form_data = URI.encode_www_form(form_data)
+
+        expect(subject.to_http).to eq(
+          [
+            "GET #{url.request_uri} HTTP/1.1",
+            'Content-Type: x-www-form-urlencoded',
+            '',
+            encoded_form_data,
+            ''
+          ].join("\r\n")
+        )
+      end
+    end
+  end
+end
