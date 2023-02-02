682269f10f7619f5979f2ffb2f76ad9c58fc2b1d
Postmodern <postmodern.mod3@gmail.com>
2023-02-02 11:14:46 -0800

    Updated the copyright years.

--- README.md
+++ README.md
@@ -307,7 +307,7 @@ gem.add_dependency 'ronin-vulns', '~> 0.1'
 
 ## License
 
-Copyright (c) 2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+Copyright (c) 2022-2023 Hal Brodigan (postmodern.mod3 at gmail.com)
 
 ronin-vulns is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published
--- lib/ronin/vulns/reflected_xss.rb
+++ lib/ronin/vulns/reflected_xss.rb
@@ -3,7 +3,7 @@
 #
 # ronin-vulns - A Ruby library for blind vulnerability testing.
 #
-# Copyright (c) 2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+# Copyright (c) 2022-2023 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
 # ronin-vulns is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
