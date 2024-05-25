7fa453a3b77f472b24bafac438a6add09bab51b5
Postmodern <postmodern.mod3@gmail.com>
2024-05-24 22:27:51 -0700

    Fix Open Redirect meta refresh regex for when `url=` is not quoted (closes #74).

--- lib/ronin/vulns/open_redirect.rb
+++ lib/ronin/vulns/open_redirect.rb
@@ -95,9 +95,9 @@ module Ronin
                 http-equiv\s*=\s*(?: "refresh" | 'refresh' | refresh )\s+
                 content\s*=\s*
                 (?:
-                 "\s*\d+\s*;\s*url\s*=\s*'\s*#{escaped_test_url}\s*'\s*"|
-                 '\s*\d+\s*;\s*url\s*=\s*"\s*#{escaped_test_url}\s*"\s*'|
-                 \s*\d+;url=(?: "#{escaped_test_url}" | '#{escaped_test_url}' )
+                 "\s*\d+\s*;\s*url\s*=\s*(?: '\s*#{escaped_test_url}\s*' | #{escaped_test_url} )\s*"|
+                 '\s*\d+\s*;\s*url\s*=\s*(?: "\s*#{escaped_test_url}\s*" | #{escaped_test_url} )\s*'|
+                 \s*\d+;url=(?: "#{escaped_test_url}" | '#{escaped_test_url}' | #{escaped_test_url} )
                 )\s*
                 (?:/\s*)?>
             }xi
--- spec/open_redirect_spec.rb
+++ spec/open_redirect_spec.rb
@@ -437,6 +437,27 @@ describe Ronin::Vulns::OpenRedirect do
                 expect(subject.vulnerable?).to be_truthy
               end
             end
+
+            context "and the url value is not quoted" do
+              let(:response_body) do
+                <<~HTML
+                  <html>
+                    <head>
+                      <meta http-equiv="refresh" content='0;url=#{subject.test_url}'/>
+                    </head>
+                    <body>
+                      <p>example content</p>
+                      <p>included content</p>
+                      <p>more content</p>
+                    </body>
+                  </html>
+                HTML
+              end
+
+              it "must return true" do
+                expect(subject.vulnerable?).to be_truthy
+              end
+            end
           end
 
           context "when the content attribute is double quoted" do
@@ -446,6 +467,27 @@ describe Ronin::Vulns::OpenRedirect do
                   <html>
                     <head>
                       <meta http-equiv="refresh" content="0;url='#{subject.test_url}'"/>
+                    </head>
+                    <body>
+                      <p>example content</p>
+                      <p>included content</p>
+                      <p>more content</p>
+                    </body>
+                  </html>
+                HTML
+              end
+
+              it "must return true" do
+                expect(subject.vulnerable?).to be_truthy
+              end
+            end
+
+            context "and the url value is not quoted" do
+              let(:response_body) do
+                <<~HTML
+                  <html>
+                    <head>
+                      <meta http-equiv="refresh" content="0;url=#{subject.test_url}"/>
                     </head>
                     <body>
                       <p>example content</p>
@@ -490,6 +532,27 @@ describe Ronin::Vulns::OpenRedirect do
                   <html>
                     <head>
                       <meta http-equiv="refresh" content=0;url='#{subject.test_url}'/>
+                    </head>
+                    <body>
+                      <p>example content</p>
+                      <p>included content</p>
+                      <p>more content</p>
+                    </body>
+                  </html>
+                HTML
+              end
+
+              it "must return true" do
+                expect(subject.vulnerable?).to be_truthy
+              end
+            end
+
+            context "and the url value is not quoted" do
+              let(:response_body) do
+                <<~HTML
+                  <html>
+                    <head>
+                      <meta http-equiv="refresh" content=0;url=#{subject.test_url}/>
                     </head>
                     <body>
                       <p>example content</p>
