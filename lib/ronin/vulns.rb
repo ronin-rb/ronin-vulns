373720913021c281db28153701754c31fad84f52
Postmodern <postmodern.mod3@gmail.com>
2024-08-22 08:25:15 -0700

    Add missing `ronin/vulns` file for `ronin-vulns irb`.

---
+++ lib/ronin/vulns.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+#
+# ronin-vulns - A Ruby library for blind vulnerability testing.
+#
+# Copyright (c) 2022-2024 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# ronin-vulns is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-vulns is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-vulns.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require_relative 'vulns/vuln'
+require_relative 'vulns/web_vuln'
+require_relative 'vulns/lfi'
+require_relative 'vulns/rfi'
+require_relative 'vulns/sqli'
+require_relative 'vulns/ssti'
+require_relative 'vulns/command_injection'
+require_relative 'vulns/open_redirect'
+require_relative 'vulns/reflected_xss'
+require_relative 'vulns/url_scanner'
+require_relative 'vulns/importer'
+require_relative 'vulns/version'
