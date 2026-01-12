b5129e149b10898dd54c90932c2f518e55f0c571
Postmodern <postmodern.mod3@gmail.com>
2026-01-11 21:51:08 -0800

    Updated the copyright year for a new file added in 0.3.0.

--- lib/ronin/vulns/cli/text.rb
+++ lib/ronin/vulns/cli/text.rb
@@ -2,7 +2,7 @@
 #
 # ronin-vulns - A Ruby library for blind vulnerability testing.
 #
-# Copyright (c) 2022-2025 Hal Brodigan (postmodern.mod3 at gmail.com)
+# Copyright (c) 2022-2026 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
 # ronin-vulns is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
